1ceb10c9b2277bda1c3d46a008992e1f5c7bd19f
Kevin Bowrin <kevin.bowrin@carleton.ca>
2019-07-26 14:36:36 -0400

    Clean up mailer initializer

--- roles/hyrax/templates/mail_initializer.rb
+++ roles/hyrax/templates/mail_initializer.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   ActionMailer::Base.smtp_settings = { address: "{{ hyrax_smtp_address }}",
                                        port: {{ hyrax_smtp_port }},
-                                       domain: {{ hyrax_smtp_domain }},
+                                       domain: "{{ hyrax_smtp_domain }}",
 {% if hyrax_smtp_user_name|length %}                                       user_name: "{{ hyrax_smtp_user_name }}",{% endif %}
 {% if hyrax_smtp_password|length %}                                       password: "{{ hyrax_smtp_password }}",{% endif %}
                                        authentication: {{ hyrax_smtp_authentication }},
