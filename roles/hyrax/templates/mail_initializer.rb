859125aa8f454addb9b2cf5d0616fde5fe292dea
Kevin Bowrin <kevin.bowrin@carleton.ca>
2019-07-26 14:44:50 -0400

    Clean up mailer initializer

--- roles/hyrax/templates/mail_initializer.rb
+++ roles/hyrax/templates/mail_initializer.rb
@@ -4,10 +4,10 @@ Rails.application.configure do
                                        domain: "{{ hyrax_smtp_domain }}",
 {% if hyrax_smtp_user_name|length %}                                       user_name: "{{ hyrax_smtp_user_name }}",{% endif %}
 {% if hyrax_smtp_password|length %}                                       password: "{{ hyrax_smtp_password }}",{% endif %}
-                                       authentication: {{ hyrax_smtp_authentication }},
-                                       enable_starttls_auto: {{ hyrax_smtp_enable_starttls_auto }},
-                                       openssl_verify_mode: {{ hyrax_smtp_openssl_verify_mode }},
-                                       ssl: {{ hyrax_smtp_ssl }},
-                                       tls: {{ hyrax_smtp_tls }}
+                                       authentication: {{ hyrax_smtp_authentication | lower }},
+                                       enable_starttls_auto: {{ hyrax_smtp_enable_starttls_auto | lower }},
+                                       openssl_verify_mode: {{ hyrax_smtp_openssl_verify_mode | lower }},
+                                       ssl: {{ hyrax_smtp_ssl | lower }},
+                                       tls: {{ hyrax_smtp_tls | lower }}
                                      }
 end
