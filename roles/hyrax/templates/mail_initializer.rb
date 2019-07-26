27cab98d2ea987a6bb7886eb8245500d9352b347
Kevin Bowrin <kevin.bowrin@carleton.ca>
2019-07-26 14:24:03 -0400

    Only include username, email in smtp config if not empty

--- roles/hyrax/templates/mail_initializer.rb
+++ roles/hyrax/templates/mail_initializer.rb
@@ -2,8 +2,8 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = { address: {{ hyrax_smtp_address }},
                                        port: {{ hyrax_smtp_port }},
                                        domain: {{ hyrax_smtp_domain }},
-                                       user_name: {{ hyrax_smtp_user_name }},
-                                       password: {{ hyrax_smtp_password }},
+                                       {% if hyrax_smtp_user_name|length %}user_name: {{ hyrax_smtp_user_name }},{% endif %}
+                                       {% if hyrax_smtp_password|length %}password: {{ hyrax_smtp_password }},{% endif %}
                                        authentication: {{ hyrax_smtp_authentication }},
                                        enable_starttls_auto: {{ hyrax_smtp_enable_starttls_auto }},
                                        openssl_verify_mode: {{ hyrax_smtp_openssl_verify_mode }},
