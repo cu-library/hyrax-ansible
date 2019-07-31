a8a914e94ab3cacaefcb0dc44c1a781660fb52c6
Kevin Bowrin <kevin.bowrin@carleton.ca>
2019-07-31 13:03:14 -0400

    Fix bad umask in backup script

--- roles/hyrax/templates/hyrax_daily_backup.sh
+++ roles/hyrax/templates/hyrax_daily_backup.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-umask 640
+umask u=rw,g=r,o= 
 
 # Delete older backups
 (echo -n "$(date --rfc-3339=seconds) - deleting older daily backups - " && find "{{ hyrax_backups_directory }}/daily" -mindepth 1 -maxdepth 1 -mtime +7 -name '*.tar.gz' -type f -delete -print | grep --color=never 'tar.gz' || echo "none found") >> /var/log/hyrax/backup.log 2>&1
